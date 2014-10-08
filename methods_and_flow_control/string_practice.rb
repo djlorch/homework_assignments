5bda773c3e74bd7b55409d4f8fc00ba4d905e843
Dan Lorch <dan.lorch@gmail.com>
2014-10-07 22:37:30 -0600

    added 2 files

---
+++ methods_and_flow_control/string_practice.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+string = 'this is a string to practice with'
+
+puts string
+puts string.capitalize
+puts string.upcase
+puts string.capitalize.gsub('string', "'string'")
+puts "The string #{string} has #{string.length} characters"
+puts string.reverse
+puts string.replace('practice ' * 3).insert(26, '!')
