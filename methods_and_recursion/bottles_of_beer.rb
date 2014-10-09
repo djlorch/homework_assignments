4e946f6bc76cbc55c50e78dbf47f9325f074d6b9
Dan Lorch <dan.lorch@gmail.com>
2014-10-08 20:49:30 -0600

    added folder

---
+++ methods_and_recursion/bottles_of_beer.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+A vacation is when you take a trip to some ADD place with your ADD family. Usually you go to some place that is near a/an ADD or up on a/an ADD. A good vacation place is one where you can ride ADD
+or play ADD or go hunting for ADD. I like  to spend my time ADD or ADD. When parents go on a vacation, they spend their time eating three ADD a day, and fathers play golf, and mothers
+                            sit around ADD. Last summer, my little brother fell in a/an ADD and got poison ADD all over his ADD. My family is going to go to (the) ADD, and I will practice ADD. Parents need vacations more than kids because parents are always very ADD and because they have to work ADD hours every day all year making enough ADD to pay for the vacation.
