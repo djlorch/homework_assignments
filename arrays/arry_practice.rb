41efa2199edab87647dde8782b03f1d9825b7a3e
Dan Lorch <dan.lorch@gmail.com>
2014-10-09 22:48:04 -0600

    upadeted files

--- arrays/arry_practice.rb
+++ arrays/arry_practice.rb
@@ -1,1 +1,17 @@
-#!usr/bin/env ruby
+#!/usr/bin/env ruby
+
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+puts "#{number.join('...')}..."
+puts "T-#{number.reverse.join(', ')}...  BLASTOFF!}"
+puts "The last element is #{number.last}"
+puts "The first element is #{number.first}"
+puts "The third element is #{number[2]}"
+puts "The element with an index of #{number[2]} is #{number[3]}"
+puts "The second form the last element #{number[-2]}"
+puts "The first four elements are '#{number.take(4).join(', ')}'"
+puts "If we delete #{number.delete(5)}, #{number.delete(6)} and #{number.delete(7)} from the array, we're left with #{number.to_s} "
+puts "If we add #{number.insert(0, 5).first} at the beginning of the array, we're left with #{number.to_s}"
+puts "If we add #{number.push(6).last} at the end of the array, we're left with #{number.to_s}"
+puts "Only the elements #{number.sort.last(2)} are > #{number.bsearch {|x| x >= 7}}."
+puts "If we remove all the elements, then the length of the array is #{number = [0].join}"
--- arrays/fizz_buzz_arry.rb
+++ arrays/fizz_buzz_arry.rb
@@ -1,1 +1,7 @@
 #!/usr/bin/env ruby
+
+numbers = (1..100).to_a
+
+buzz_kill = [number % 3 == 0 && number % 5 == 0 puts 'FizzBuzz', number % 3 == 0 puts 'Fizz',number % 5 == 0 puts 'Buzz', numbers]
+
+puts numbers
